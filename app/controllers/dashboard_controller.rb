85f3fb7899c8ae329f476139800506e76dd2e806
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-08-22 13:54:51 -0500

    Show all user starred repos

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,12 @@
 class DashboardController < ApplicationController
   def index
-    client = Octokit::Client.new(:access_token => current_user.token)
+    # Call the Octokit Ruby Client with opts
+    client = Octokit::Client.new(client_id: ENV['GITHUB_KEY'], client_secret: ENV['GITHUB_SECRET'], :access_token => current_user.token, auto_paginate: true)
 
-    @user = client.user
-    # raise :test
+    # Assign the client result to the user variable
+    user = client
 
-    @starred_repos_list = Octokit.starred(@user.login, :per_page => 100)
-    # puts "#{@starred_repos_list}"
+    # Assign the user starred repos into the @starred_repos_list variable
+    @starred_repos_list = user.starred
   end
 end
