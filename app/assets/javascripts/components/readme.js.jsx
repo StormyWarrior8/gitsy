c6425cab09bd14a10b4e3d9de2dda037445dc525
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-08-24 11:03:03 -0500

    Refactor Readme component

--- app/assets/javascripts/components/readme.js.jsx
+++ app/assets/javascripts/components/readme.js.jsx
@@ -1,40 +1,26 @@
 var Readme = React.createClass({
-  renderRepoName: function(repo) {
-    var name = "";
-
-    repo.map((r) => {
-      name = r.name;
-    });
-
-    return name;
-  },
-  renderRepoClone(repo) {
-    var clone = "";
-
-    repo.map((r) => {
-      clone = r.cloneUrl;
-    });
-
-    return clone;
-  },
-  renderRepoReadme(repo) {
-    var readme = "";
-
-    repo.map((r) => {
-      readme = r.readme;
-    });
-
-    return readme;
-  },
   render: function() {
     var repo = this.props.repo;
+
+    var name = '';
+    var clone = '';
+    var readme = '';
+
+    var assignRepoData = (repo) => {
+      return repo.map((r) => {
+        name = r.name;
+        clone = r.cloneUrl;
+        readme = r.readme;
+      });
+    };
 
     return (
       <div className="ten wide column">
         <div className="ui piled very padded text container segment">
-          <h1>{this.renderRepoName(repo)}</h1>
-          <p>{this.renderRepoClone(repo)}</p>
-          <h3>{this.renderRepoReadme(repo)}</h3>
+          {assignRepoData(repo)}
+          <h1>{name}</h1>
+          <p>{clone}</p>
+          <h3>{readme}</h3>
         </div>
       </div>
     );
