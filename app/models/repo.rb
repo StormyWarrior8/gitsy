939edb689f6e23ebb5da4e960be148e0f175c338
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-08-24 22:39:08 -0500

    Build the repos readme_url and pass em as json

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,5 +1,34 @@
 class Repo < ApplicationRecord
+  # require 'net/http'
+  # require 'uri'
+
   belongs_to :user
+
+  # def self.open(url)
+  #   if url != "Tell this guy to write a README, please."
+  #     return Net::HTTP.get(URI.parse(url))
+  #   else
+  #     return url
+  #   end
+  # end
+
+  def self.build_url(repo_name)
+    return "https://raw.githubusercontent.com/"+repo_name+"/master/README.md"
+  end
+
+  # def self.get_readme(repo_name)
+  #   pre_uri = Repo.build_url(repo_name)
+  #   uri = URI(pre_uri)
+  #   uri_res = Net::HTTP.get_response(uri)
+  #
+  #   if uri_res.code == "200"
+  #     url = pre_uri
+  #   else
+  #     url = "Tell this guy to write a README, please."
+  #   end
+  #
+  #   Repo.open(url)
+  # end
 
   def self.get_correlation(user, id)
     user.repos.each do |r|
@@ -13,17 +42,16 @@ class Repo < ApplicationRecord
 
   def self.get_client_repos(user, user_token)
     client = Octokit::Client.new(client_id: ENV['GITHUB_KEY'], client_secret: ENV['GITHUB_SECRET'], :access_token => user_token, auto_paginate: true)
-
     repos = []
 
     if user.repos.count > 0
       client.starred.each do |r|
-        repo = {"id" => r.id, "repo_name" => r.full_name, "desc" => r.description, "lang" => r.language, "html_url" => r.html_url, "clone_url" => r.clone_url, "stars" => r.stargazers_count, "forks" => r.forks_count, "watchers" => r.watchers, "star_level" => Repo.get_correlation(user, r.id)}
+        repo = {"id" => r.id, "repo_name" => r.full_name, "desc" => r.description, "lang" => r.language, "html_url" => r.html_url, "clone_url" => r.clone_url, "stars" => r.stargazers_count, "forks" => r.forks_count, "watchers" => r.watchers, "readme_url" => Repo.build_url(r.full_name) , "star_level" => Repo.get_correlation(user, r.id)}
         repos << repo
       end
     else
       client.starred.each do |r|
-        repo = {"id" => r.id, "repo_name" => r.full_name, "desc" => r.description, "lang" => r.language, "html_url" => r.html_url, "clone_url" => r.clone_url, "stars" => r.stargazers_count, "forks" => r.forks_count, "watchers" => r.watchers, "star_level" => 1}
+        repo = {"id" => r.id, "repo_name" => r.full_name, "desc" => r.description, "lang" => r.language, "html_url" => r.html_url, "clone_url" => r.clone_url, "stars" => r.stargazers_count, "forks" => r.forks_count, "watchers" => r.watchers, "readme_url" => Repo.build_url(r.full_name), "star_level" => 1}
         repos << repo
       end
     end
