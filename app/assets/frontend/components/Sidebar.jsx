b96471dc978b57c342de39870bbd831aa977ccc1
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-09-02 17:55:44 -0500

    Add global/local filters in Sidebar component

--- app/assets/frontend/components/Sidebar.jsx
+++ app/assets/frontend/components/Sidebar.jsx
@@ -4,11 +4,19 @@ var TagList = require('TagList');
 var Sidebar = React.createClass({
   render: function() {
     return (
-      <div className="sidebar">
-        <h4 className="sidebar-title">
+      <div className="Sidebar">
+        <h4 className="Sidebar-title">
           <i className="fa fa-tags" aria-hidden={true}></i>&nbsp;Tag Center
         </h4>
-        <div className="sidebar-container">
+        <div className="Sidebar-filters">
+          <div className="Sidebar-filters__global">
+            <span>Global</span>
+          </div>
+          <div className="Sidebar-filters__local">
+            <span>Local</span>
+          </div>
+        </div>
+        <div className="Sidebar-container">
           <TagList repos={this.props.repos} onClickTag={this.props.onClickTag}/>
         </div>
       </div>
