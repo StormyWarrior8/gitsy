d3ebe864c4f2ab1a19e5a46893e3f0d5cf72a771
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-09-02 00:07:14 -0500

    Add jsx comments and fix js comments

--- app/assets/frontend/components/Dashboard.jsx
+++ app/assets/frontend/components/Dashboard.jsx
@@ -20,10 +20,11 @@ var Dashboard = React.createClass({
     }
   },
   componentDidMount: function () {
-    {/* currentStarredRepos will be the ones showing up in the ui */}
-    {/* while allStarredRepos will serve to just look up and update currentStarredRepos when search/filter queries occur */}
+    /* currentStarredRepos will be the ones showing up in the ui */
+    /* while allStarredRepos will serve to just look up and update currentStarredRepos when search/filter queries occur */
     $.getJSON('/repos', (response) => {this.setState({ currentStarredRepos: response, allStarredRepos: response })})
   },
+  /* handle repo star levels */
   handleStar: function(id) {
     var updatedRepos = this.state.currentStarredRepos.map((repo) => {
       if (repo.id === id) {
@@ -66,6 +67,7 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* show selected repo info */
   handleRepo: function(repo_id, clone_url, readme_url) {
     this.setState({
       selectedRepoCloneUrl: clone_url,
@@ -74,6 +76,7 @@ var Dashboard = React.createClass({
       repoActivated: true
     });
   },
+  /* tagging functionality */
   handleAddTag: function(text) {
     var updatedRepos = this.state.currentStarredRepos.map((repo) => {
       if (repo.id === this.state.selectedRepoId) {
@@ -99,6 +102,7 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* filter by one star repos */
   setOneStar: function(one) {
     var updatedRepos = [];
     this.state.allStarredRepos.map((repo) => {
@@ -109,6 +113,7 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* filter by two stars repos */
   setTwoStars: function(two) {
     var updatedRepos = [];
     this.state.allStarredRepos.map((repo) => {
@@ -119,6 +124,7 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* filter by three stars repos */
   setThreeStars: function(three) {
     var updatedRepos = [];
     this.state.allStarredRepos.map((repo) => {
@@ -129,10 +135,12 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* reset filters and show all repos */
   resetStars: function() {
     var allRepos = this.state.allStarredRepos;
     this.setState({currentStarredRepos: allRepos});
   },
+  /* show repos tagged with a specific tag */
   handleOnClickTag: function(tag) {
     var updatedRepos = [];
     this.state.allStarredRepos.map((repo) => {
@@ -143,6 +151,7 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* filter by tagged repos */
   handleOnClickTagged: function() {
     var updatedRepos = [];
     this.state.allStarredRepos.map((repo) => {
@@ -153,6 +162,7 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* filter by untagged repos */
   handleOnClickUntagged: function() {
     var updatedRepos = [];
     this.state.allStarredRepos.map((repo) => {
@@ -165,17 +175,20 @@ var Dashboard = React.createClass({
 
     this.setState({currentStarredRepos: updatedRepos});
   },
+  /* global search functionality */
   handleGlobalSearch: function(searchText) {
     this.setState({
       globalSearchText: searchText.toLowerCase()
     });
   },
+  /* local search functionality */
   handleSearch: function(searchText) {
     this.setState({
       searchText: searchText.toLowerCase()
     });
   },
   render: function() {
+    {/* state vars */}
     var allStarredRepos = this.state.allStarredRepos;
     var currentStarredRepos = this.state.currentStarredRepos;
     var selectedRepoCloneUrl = this.state.selectedRepoCloneUrl;
@@ -184,10 +197,14 @@ var Dashboard = React.createClass({
     var searchText = this.state.searchText;
     var globalSearchText = this.state.globalSearchText;
 
-    var filteredRepos = RepoAPI.filterRepos(allStarredRepos, currentStarredRepos, globalSearchText, searchText);
+    {/* props vars */}
     var userName = this.props.userName;
     var userAvatar = this.props.userAvatar;
 
+    {/* repo api call */}
+    var filteredRepos = RepoAPI.filterRepos(allStarredRepos, currentStarredRepos, globalSearchText, searchText);
+
+    {/* show readme */}
     var showReadme = (active) => {
       if (active == true) {
         return <Readme selectedRepoClone={selectedRepoCloneUrl} selectedRepoReadme={selectedRepoReadmeUrl} onAddTag={this.handleAddTag}/>;
